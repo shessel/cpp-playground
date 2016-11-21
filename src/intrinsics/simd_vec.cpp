f2ab0e270302e98091da35065016903830cbf6b7
shessel <shessel@users.noreply.github.com>
2016-11-21 22:26:58 +0100

    add matrix and matrix vector multiplication operator

--- src/intrinsics/simd_vec.cpp
+++ src/intrinsics/simd_vec.cpp
@@ -45,6 +45,24 @@ std::ostream& operator<<(std::ostream& os, const vec4d& vec) {
     return os;
 }
 
+struct mat4f {
+    __m128 row0;
+    __m128 row1;
+    __m128 row2;
+    __m128 row3;
+
+    explicit mat4f(__m128 row0, __m128 row1, __m128 row2, __m128 row3) :
+        row0(row0), row1(row1), row2(row2), row3(row3) {}
+    explicit mat4f(vec4f row0, vec4f row1, vec4f row2, vec4f row3) :
+        row0(row0.data.raw), row1(row1.data.raw), row2(row2.data.raw), row3(row3.data.raw) {}
+    vec4f operator*(const vec4f& vec) const {
+        return _mm_or_ps(_mm_dp_ps(row0, vec.data.raw, 0xF1),
+               _mm_or_ps(_mm_dp_ps(row1, vec.data.raw, 0xF2),
+               _mm_or_ps(_mm_dp_ps(row2, vec.data.raw, 0xF4),
+                         _mm_dp_ps(row3, vec.data.raw, 0xF8))));
+    }
+};
+
 float dot(const vec4f& a, const vec4f& b) {
     __m128 dp = _mm_dp_ps(a.data.raw, b.data.raw, 0xF1);
     return _mm_cvtss_f32(dp);
@@ -61,6 +79,25 @@ vec4f dot4(const std::array<vec4f,4>& a, const std::array<vec4f, 4>& b) {
 void testDot() {
     std::cout << "dot: " << dot(vec4f{1.0f, 1.0f, 0.0f, 0.0f}, vec4f{1.0f, 1.0f, 0.0f, 0.0f}) << std::endl;
     std::cout << "dot: " << dot(vec4f{2.0f, 3.0f, 0.0f, 0.0f}, vec4f{5.0f, 5.0f, 0.0f, 0.0f}) << std::endl;
+}
+
+void testMat4f() {
+    const mat4f id {{1.0f, 0.0f, 0.0f, 0.0f},
+                    {0.0f, 1.0f, 0.0f, 0.0f},
+                    {0.0f, 0.0f, 1.0f, 0.0f},
+                    {0.0f, 0.0f, 0.0f, 1.0f}};
+
+    const mat4f shuffle {{0.0f, 0.0f, 1.0f, 0.0f},
+                         {0.0f, 0.0f, 0.0f, 1.0f},
+                         {0.0f, 1.0f, 0.0f, 0.0f},
+                         {1.0f, 0.0f, 0.0f, 0.0f}};
+
+    const vec4f vec{1.0f, 2.0f, 3.0f, 4.0f};
+    const vec4f unchanged = id * vec;
+    const vec4f shuffled = shuffle * vec;
+
+    std::cout << "testMat4f: " << unchanged << std::endl;
+    std::cout << "testMat4f: " << shuffled << std::endl;
 }
 
 void testDot4() {
@@ -101,6 +138,7 @@ int main() {
     testDot();
     testDot4();
     testSetLoadMulAdd();
+    testMat4f();
 
     return 0;
 }
